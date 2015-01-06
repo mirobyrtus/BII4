8056bcdcfed4a0f450c25702467fdddf3e283fe8
leo <tuwien@ist-einmalig.de>
2015-01-06 23:41:20 +0100

    First approach 2 display data

---
+++ RemoteBluetoothServer/src/at/bii/display/FFT_Display.java
@@ -0,0 +1,55 @@
+package at.bii.display;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class FFT_Display extends JFrame {
+	JPanel myPaint = new JPanel();
+	private int[] spectrum;
+
+	public FFT_Display() {
+		setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
+		myPaint.setBackground(Color.PINK);
+		getContentPane().add(myPaint);
+		setSize(new Dimension(800, 600));
+		setVisible(true);
+	}
+
+	@Override
+	public void setVisible(boolean b) {
+		super.setVisible(b);
+		if (!b) {
+			System.exit(0);
+		}
+	}
+	
+	public void displayData(int[] spectrum) {
+		this.spectrum=spectrum;
+		paintComponents(getGraphics());
+	}
+	
+	@Override
+	public void paintComponents(Graphics g) {
+		super.paintComponents(g);
+
+		int width = getWidth();
+		int height = getHeight();
+
+		int barWidth = width / spectrum.length ;
+
+		for (int i = 0; i < spectrum.length ; i++) {
+			int curHeight = spectrum[i]*height/ 0xffffffff;
+			g.drawRect(i*barWidth,0,barWidth,curHeight);  
+		    g.setColor(Color.green);  
+			g.fillRect(i*barWidth,0,barWidth,curHeight);  
+		}
+	}
+
+	
+	
+}
